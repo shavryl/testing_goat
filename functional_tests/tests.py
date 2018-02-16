57352b8730141d9f84d2b60ff2736fc12085dd15
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-02-16 21:44:16 +0000

    first steps of FT for styling layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,8 +10,6 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
-
-
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -74,8 +72,6 @@ class NewVisitorTest(LiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-        self.fail('Finish the test!')
-
 
     def test_different_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -119,3 +115,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the inputbox is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=10)
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
