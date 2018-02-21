1f57bc9a7df1c1bd8f3a3e0ffc9a3ccabffd5539
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-02-21 08:35:34 +0000

    Moved db outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
+        staging_server = 'thereshouldbethename.club' #os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
 
--- testing_goat/settings.py
+++ testing_goat/settings.py
@@ -74,10 +74,11 @@ WSGI_APPLICATION = 'testing_goat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
