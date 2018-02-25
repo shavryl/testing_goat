2c2daa4c8df387ae875366ea8a6db97e995d408e
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-02-25 20:58:11 +0000

    list item validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,6 @@ class ItemValidationTest(FunctionalTest):
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         self.wait_for(lambda: self.assertEqual(
