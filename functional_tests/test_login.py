a62cbdde398972c0d76ffa8b5de3422d88fa2ca6
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-03-02 09:19:58 +0000

    precreate sessions, move login checks into base, state 2FT fails

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -26,34 +26,38 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
-
-        # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
-        if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
-        url = url_search.group(0)
-        self.assertIn(self.live_server_url, url)
-
-        # she clicks it
-        self.browser.get(url)
+        # email = mail.outbox[0]
+        # self.assertIn(TEST_EMAIL, email.to)
+        # self.assertEqual(email.subject, SUBJECT)
+        #
+        # # It has a url link in it
+        # self.assertIn('Use this link to log in', email.body)
+        # url_search = re.search(r'http://.+/.+$', email.body)
+        # if not url_search:
+        #     self.fail(f'Could not find url in email body:\n{email.body}')
+        # url = url_search.group(0)
+        # self.assertIn(self.live_server_url, url)
+        #
+        # # she clicks it
+        # self.browser.get(url)
 
         # she is logged in!
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+        # self.wait_for(
+        #     lambda: self.browser.find_element_by_link_text('Log out')
+        # )
+        # navbar = self.browser.find_element_by_css_selector('.navbar')
+        # self.assertIn(TEST_EMAIL, navbar.text)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # She is logged out
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+        # self.wait_for(
+        #     lambda: self.browser.find_element_by_name('email')
+        # )
+        # navbar = self.browser.find_element_by_css_selector('.navbar')
+        # self.assertNotIn(TEST_EMAIL, navbar.text)
